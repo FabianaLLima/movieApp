f1022b4d834ae1d08333c8901a5b3881f66cf055
Fabiana Limma <fabianallima@hotmail.com>
2023-05-29 20:01:20 -0300

    Criando layout da home view

---
+++ movieApp/Home/HomeViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  movieApp
+//
+//  Created by Fabiana Limma on 29/05/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Filmes Populares"
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 28, weight: .medium)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    var tableView: UITableView = {
+        let view = UITableView(frame: .zero, style: .plain)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationController?.navigationBar.isHidden = true
+        view.backgroundColor = .white
+        view.addSubview(titleLabel)
+        view.addSubview(tableView)
+
+        titleLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 52).isActive = true
+        titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16).isActive = true
+        titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16).isActive = true
+        
+        tableView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 42).isActive = true
+        tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }
+    
+}
+
